c4bb86c5fb28d54902f77ceaf2b29cf906bd46e0
LucianoTrujillo <luckytrujillo123@gmail.com>
2022-05-08 22:11:02 -0300

    added basic structure for parser (#14)
    * added basic structure for parser
    
    * formatted code
    
    Co-authored-by: Luciano trujillo <luciano.trujillo@innovid.com>

---
+++ src/config/parser.rs
@@ -0,0 +1,3 @@
+pub fn parse_from_path(config_path: &str) {
+    println!("{}", config_path);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use bittorrent_rustico::config::parser;
 fn main() {
+    parser::parse_from_path("HOLA");
     println!("Hello, world!");
 }
